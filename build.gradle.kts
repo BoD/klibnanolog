3d19267d2bce1654f445597f99b28fa1d5e1d756
Benoit 'BoD' Lubek <BoD@JRAF.org>
2026-04-25 10:35:07 +0200

    Prepare snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,5 @@ Librarian.root(project)
 // `./gradlew refreshVersions` to update dependencies
 // `./gradlew publishToMavenLocal` to publish to local Maven repository
 // `LIBRARIAN_SONATYPE_USERNAME=xxxx LIBRARIAN_SONATYPE_PASSWORD=xxxx LIBRARIAN_SIGNING_PRIVATE_KEY=`cat /path/to/armored/ascii/private-key.gpg` LIBRARIAN_SIGNING_PRIVATE_KEY_PASSWORD=xxxx ./gradlew librarianPublishToMavenCentral` to publish to Maven Central
+
+// To make a release: create a tag, this will trigger a build which will push the artifact and create the release on GitHub.
